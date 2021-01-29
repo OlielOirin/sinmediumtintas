6779c012ad5cf2e650047c305f6c3d13c9f9989c
OlielOirin <75178605+OlielOirin@users.noreply.github.com>
2021-01-29 01:31:29 +0100

    sinmediumtintas in

---
+++ assets/js/hablemos.js
@@ -0,0 +1,1 @@
+function modalOut(){modal_bg.style.display="none"}function timerModal(){setTimeout(function(){modal_bg.style.display="flex",modal_bg.classList.add("animate")},1500)}var submit=document.getElementById("submit"),modal_bg=document.getElementById("h-modal"),modal_text=document.getElementById("modal2");
